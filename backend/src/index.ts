f5f0eed39628d0a15257132f4e45e9b532dfd973
manish-107 <creativecrew2676@gmail.com>
2024-06-18 12:25:02 +0530

    signin jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 // import {userRoutes} from 'routes/users.ts'
 
 const app = new Hono<{
   Bindings:{
     DATABASE_URL:string;
+    JWT_SECRET:string;
   }
 }>()
 
@@ -41,8 +43,43 @@ app.post('/api/v1/signup', async (c) => {
         email:true
       }
 		});
+
+    const jwt = await sign({
+        id:user.id
+    },c.env.JWT_SECRET)
 	
-		return c.json({data:user})
+		return c.json({data:user,jwt:jwt})
+	} catch(e) {
+    c.status(403);
+		return c.json({error:e});
+	}
+})
+app.post('/api/v1/signin', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.findFirst({
+      where:{
+        email:body.email,
+        password:body.password,
+      }
+    });
+
+    if(!user){
+      c.status(403);
+		  return c.json({message:"Email doesnt exists"})
+    }
+
+    const jwt = await sign({
+      id:user.id
+    },c.env.JWT_SECRET);
+    return c.json({
+      jwt:jwt
+    })
+  
 	} catch(e) {
     c.status(403);
 		return c.json({error:e});

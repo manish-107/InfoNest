455ed5b4c1c3d49897e41506b479eac60dd529b8
manish-107 <creativecrew2676@gmail.com>
2024-06-18 10:50:30 +0530

    get and post request

--- backend/prisma/migrations/20240618051309_init_schema/migration.sql
+++ backend/prisma/migrations/20240618051309_init_schema/migration.sql
@@ -2,8 +2,9 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT,
+    "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,52 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 // import {userRoutes} from 'routes/users.ts'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string;
+  }
+}>()
 
-// app.route('/user',userRoutes);
-
-app.get('/',(c)=>{
-  return c.text('hello');
+app.get('/api/v1/getAllUsers', async(c)=>{
+  const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+  try {
+    const AllUser = await prisma.user.findMany({})
+    return c.json({users:AllUser});
+  } catch (error) {
+   
+    c.status(403);
+    return c.json({error:error});
+  }
 })
 
-
+app.post('/api/v1/signup', async (c) => {
+	const prisma = new PrismaClient({
+		datasourceUrl: c.env?.DATABASE_URL,
+	}).$extends(withAccelerate());
+	const body = await c.req.json();
+	try {
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+        name:body.name,
+				password: body.password
+			},
+      select:{
+        id:true,
+        name:true,
+        email:true
+      }
+		});
+	
+		return c.json({data:user})
+	} catch(e) {
+    c.status(403);
+		return c.json({error:e});
+	}
+})
 
 export default app
